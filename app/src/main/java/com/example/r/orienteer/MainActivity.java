172ceed6601028738f27ccb091e0318b656e0250
kriss1240 <dungeonkriss@gmail.com>
2018-06-09 14:05:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/r/orienteer/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.r.orienteer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.kriss.kompass.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LegeActivity.class));
+            }
+        });
+    }
+}
